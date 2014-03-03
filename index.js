f182be2651813d552e3e4c3c7c1cc6eb3a5d01e7
Sander Nemvalts <snemvalts@gmail.com>
2014-03-03 19:36:26 +0200

    Added changing location and minor changes

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ game.player.stocks = {
 	acid: 0,
 	speed: 0,
 };
-game.regions = {
-
-}
+game.player.location = "Bronx";
+game.locations = ["Bronx","Manhattan","Central park","Manhattan","Coney Island"]
 game.prices = {
 	ludes: Math.floor(Math.random()*4+1)*10,
 	weed: Math.floor(Math.random()*42+33)*10,
@@ -30,25 +29,38 @@ game.newPrices = function(){
 	speed: Math.floor(Math.random()*15+7)*10,
 }
 }
+game.getPrice = function(drug){
+	return game.prices[drug]
+}
 game.buy = function(options){
-	var price = game.prices[options.drug];
-	var moneyBefore = game.player.money
+	var price = game.getPrice(options.drug);
+	var moneyBefore = game.player.money;
+	//if the total cost is greater than player money, subtract zero.
 	game.player.money-= price*options.quantity > game.player.money ? 0 : price*options.quantity;
-	if(game.player.money === moneyBefore) return "You don't have enough money";
+	//if no change in balance
+	if(game.player.money === moneyBefore && options.quantity !== 0) return "You don't have enough money";
 	game.player.stocks[options.drug]+=options.quantity
 	console.log("Trade complete. You have bought a total of",options.quantity,options.drug,"at $",price);
 	console.log("You have $",game.player.money)
 	return "You currently have "+game.player.stocks[options.drug]+" "+options.drug
 }
 game.sell = function(options){
-	var price = game.prices[options.drug];
+	var price = game.getPrice(options.drug);
 	var quantityBefore = game.player.stocks[options.drug];
 	game.player.stocks[options.drug]-= options.quantity > game.player.stocks[options.drug] ? 0 : options.quantity;
-	if(game.player.stocks[options.drug] === quantityBefore) return "You don't have enough of that drug.";
+	if(game.player.stocks[options.drug] === quantityBefore && options.quantity !== 0) return "You don't have enough of that drug.";
 	game.player.money+= price*options.quantity;
 	console.log("Trade complete. You have sold a total of",options.quantity,options.drug,"at $",price);
 	console.log("You have $",game.player.money)
 	return "You currently have "+game.player.stocks[options.drug]+" "+options.drug
+}
+game.move = function(location){
+	for(var i = 0; i < game.locations.length; i++){
+		if(game.locations[i] === location){
+			game.player.location = location;
+			game.newPrices();
+		}
+	}
 }
 game.trade = function(options){
 	if(typeof options.tradeType !== "string") return new Error("Trade type not specified");
@@ -62,7 +74,8 @@ game.trade = function(options){
 		return game.sell(options)
 	}
 }
-console.log(game.trade({tradeType:"buy",drug:"heroin",quantity:10}))
-console.log(game.trade({tradeType:"sell",drug:"heroin",quantity:20}))
-console.log(game.trade({tradeType:"buy",drug:"speed",quantity:1}))
-console.log(game.trade({tradeType:"sell",drug:"speed",quantity:1}))
+console.log(game.trade({tradeType:"buy",drug:"speed",quantity:5}))
+game.move("Central park");
+console.log(game.player.location)
+console.log(game.trade({tradeType:"sell",drug:"speed",quantity:5}))
+console.log(game.getPrice("cocaine"));
