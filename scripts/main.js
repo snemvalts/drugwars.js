3756f7be07c01d21eefa01da7a4b9276b4e38c37
Sander Nemvalts <snemvalts@gmail.com>
2014-03-12 19:06:49 +0200

    really small change

--- scripts/main.js
+++ scripts/main.js
@@ -14,11 +14,14 @@ $("#moveButton").click(function(){
 	game.move($("#locationInput").val())
 	$("#location span").text(game.player.location)
 })
+//actually update money on debt
 $("#getDebtButton").click(function(){
 	$("#log #status").text(game.getDebt(parseInt($("#debtInput").val())))
+	$("#log #money span").text(game.player.money)
 })
 $("#payDebtButton").click(function(){
 	$("#log #status").text(game.payDebt(parseInt($("#debtInput").val())))
+	$("#log #money span").text(game.player.money)
 })
 $("#getQuotesButton").click(function(){
 	$("#log #status").text("The drug "+$("#drugInput").val()+" costs $"+game.prices[$("#drugInput").val()])
