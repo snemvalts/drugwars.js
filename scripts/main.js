1d029fe180d2fb28ba496ccbb0334230c576fcf3
Sander Nemvalts <snemvalts@gmail.com>
2014-03-12 07:32:26 +0200

    Added buying and selling

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 $("#playButton").click(function(){
-	$("#header").toggle();
 	$("#playButton").toggle();
-	console.log("woo");
-	
+	$(".dontDisplayYet").toggle();	
+})
+$("#buyButton").click(function(){
+	$("#log").text(game.trade({tradeType:"buy",drug:$("#drugInput").val().toLowerCase(),quantity:parseInt($("#quantityInput").val())}))
 })
