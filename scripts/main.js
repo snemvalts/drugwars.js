93632271bfeb194b2b401c33ea0e1f0b4b9721cd
Sander Nemvalts <snemvalts@gmail.com>
2014-03-12 18:57:37 +0200

    Added debt system and price getting, overhauled some stuff

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,23 @@ $("#playButton").click(function(){
 	$(".dontDisplayYet").toggle();	
 })
 $("#buyButton").click(function(){
-	$("#log").text(game.trade({tradeType:"buy",drug:$("#drugInput").val().toLowerCase(),quantity:parseInt($("#quantityInput").val())}))
+	$("#log #status").text(game.trade({tradeType:"buy",drug:$("#drugInput").val().toLowerCase(),quantity:parseInt($("#quantityInput").val())}))
+	$("#log #money span").text(game.player.money)
 })
 $("#sellButton").click(function(){
-	$("#log").text(game.trade({tradeType:"sell",drug:$("#drugInput").val().toLowerCase(),quantity:parseInt($("#quantityInput").val())}))
+	$("#log #status").text(game.trade({tradeType:"sell",drug:$("#drugInput").val().toLowerCase(),quantity:parseInt($("#quantityInput").val())}))
+	$("#log #money span").text(game.player.money)
 })
 $("#moveButton").click(function(){
 	game.move($("#locationInput").val())
 	$("#location span").text(game.player.location)
+})
+$("#getDebtButton").click(function(){
+	$("#log #status").text(game.getDebt(parseInt($("#debtInput").val())))
+})
+$("#payDebtButton").click(function(){
+	$("#log #status").text(game.payDebt(parseInt($("#debtInput").val())))
+})
+$("#getQuotesButton").click(function(){
+	$("#log #status").text("The drug "+$("#drugInput").val()+" costs $"+game.prices[$("#drugInput").val()])
 })
