830550dd85043c176fb4118a119e66d98675f5c4
Sander Nemvalts <snemvalts@gmail.com>
2014-03-13 08:27:21 +0200

    Added inventory checking

--- scripts/main.js
+++ scripts/main.js
@@ -4,17 +4,18 @@ $("#playButton").click(function(){
 })
 $("#buyButton").click(function(){
 	$("#log #status").text(game.trade({tradeType:"buy",drug:$("#drugInput").val().toLowerCase(),quantity:parseInt($("#quantityInput").val())}))
+	$("#stock span").text(getInventory())
 	$("#log #money span").text(game.player.money)
 })
 $("#sellButton").click(function(){
 	$("#log #status").text(game.trade({tradeType:"sell",drug:$("#drugInput").val().toLowerCase(),quantity:parseInt($("#quantityInput").val())}))
+	$("#stock span").text(getInventory())
 	$("#log #money span").text(game.player.money)
 })
 $("#moveButton").click(function(){
 	game.move($("#locationInput").val())
 	$("#location span").text(game.player.location)
 })
-//actually update money on debt
 $("#getDebtButton").click(function(){
 	$("#log #status").text(game.getDebt(parseInt($("#debtInput").val())))
 	$("#log #money span").text(game.player.money)
@@ -26,3 +27,11 @@ $("#payDebtButton").click(function(){
 $("#getQuotesButton").click(function(){
 	$("#log #status").text("The drug "+$("#drugInput").val()+" costs $"+game.prices[$("#drugInput").val()])
 })
+
+function getInventory(){
+	var stringToSend = "";
+	for(var i in game.player.stocks){
+		stringToSend+=i+": "+game.player.stocks[i]+", ";
+	}
+	return stringToSend.substring(0,stringToSend.length-2);
+}
